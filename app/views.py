cf40e64fa2f0b6a506dd5eb6870593bdb5ea0aad
MasterEzio <111882164+sushant7326@users.noreply.github.com>
2024-07-12 00:56:15 +0530

    uploading files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
 
 @app.template_filter("clean_date")
 def clean_date(dt):
@@ -171,3 +173,47 @@ def query():
     print(request.query_string)
 
     return "No query string", 200
+
+app.config["IMAGE_UPLOADS"] = "/home/sushant/comding/flask-tut-1/app/static/img/uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
+
+def allowed_image(filename):
+    if not "." in filename:
+        return False
+    
+    ext = filename.rsplit(".",1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+    
+def allowed_image_filesize(filesize):
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+    
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            if not allowed_image_filesize(request.cookies.get("filesize")):
+                print("Maximum File Size Exceeded")
+                return redirect(request.url)
+            image = request.files["image"]
+            if image.filename == "":
+                print("Image must have a filename")
+                return redirect(request.url)
+            
+            if not allowed_image(image.filename):
+                print("Invalid File Extension")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+            print("Image Saved")
+            return redirect(request.url)
+    return render_template("public/upload_image.html")
